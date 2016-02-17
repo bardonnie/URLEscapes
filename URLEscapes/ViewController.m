9042ae32b547c99d26cbd5b2ade6d1890fadfb86
xin.chen <xin.chen@wecook.cn>
2016-02-17 11:28:22 +0800

    upload README

--- URLEscapes/ViewController.m
+++ URLEscapes/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 
+#import "NSString+URLEscapes.h"
+
 @interface ViewController ()
 
 @end
@@ -17,6 +19,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // url encode
+    NSString *urlString = @"http://www.wecook.cn?link=http://wecook.cn//egret//index.html?platInfo=open_86_18269&wy_game=170&wy_user=389&wy_bs=abc&isiframe=0&isapp=true&game=味库";
+    NSString *query = [urlString escapedURLString];
+    NSLog(@"query - %@", query);
+    
+    // ASCII in url -> url decode
+    NSString *query1 = [query originalURLString];
+    NSLog(@"query1 - %@", query1);
 }
 
 - (void)didReceiveMemoryWarning {
